b44863a750e06ca553d8114300c2a81eb5cf059f
Chaitanya Rane <68692629+chayrane@users.noreply.github.com>
2024-05-14 14:19:44 +0530

    created teams and contacts data grid

--- react-admin/src/scenes/dashboard/index.jsx
+++ react-admin/src/scenes/dashboard/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
 
 function Dashboard() {
   return (
-    <div>
-        
-    </div>
-  )
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subTitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
